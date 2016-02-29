702002263d4b3c887a23636f132cfcdfaa3b7cd0
lk <746401552@qq.com>
2016-02-29 20:04:06 +0800

    阳哥光芒万丈

---
+++ shiyan/shiyan/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  shiyan
+//
+//  Created by lk on 16/2/29.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    
+    
+    NSLog(@"我好帅");// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
